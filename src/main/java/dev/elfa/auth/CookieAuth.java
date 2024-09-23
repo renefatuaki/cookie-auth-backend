a81b6a2ed9bf8474743c88d08e1fe74c2f8a1e99
René El Fatuaki <rene@elfatuaki.com>
2024-09-23 15:20:12 +0200

    Add CookieAuth and CookieAuthTests files

---
+++ src/main/java/dev/elfa/auth/CookieAuth.java
@@ -0,0 +1,13 @@
+package dev.elfa.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieAuth {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieAuth.class, args);
+	}
+
+}
---
+++ src/test/java/dev/elfa/auth/CookieAuthTests.java
@@ -0,0 +1,14 @@
+package dev.elfa.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieAuthTests {
+
+	@Test
+	void contextLoads() {
+		// Test
+	}
+
+}
